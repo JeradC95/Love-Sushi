797778372a6c61fbbcf86fe58589ce8fa1d30173
Tiffany Ferderer <tferderer@mail.greenriver.edu>
2021-02-21 17:29:03 -0500

    Run f3 added

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php
 
 //This is my CONTROLLER
-/** Create a food order form */
 
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Start a session
-session_start();
-
 //require the autoload file
 require_once('vendor/autoload.php');
-
 
 
 //Create an instance of Base class
@@ -21,7 +16,7 @@ $f3->set('DEBUG', 3);
 
 //define a default route(home page)
 $f3->route('GET /', function() {
-    //echo "Hello";
+    echo "Hello";
     $view = new Template();
     echo $view->render('views/home.html');
 });
