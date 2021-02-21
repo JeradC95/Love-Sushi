3c55a4df796672ba66ed0282658d67cb15f61414
Jerad <Jclark74@mail.greenriver.edu>
2021-02-21 16:53:15 -0500

    Changes to index.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ session_start();
 require_once('vendor/autoload.php');
 
 
+
 //Create an instance of Base class
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
@@ -24,4 +25,32 @@ $f3->route('GET /', function() {
     //echo "Hello";
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+//define a default route(admin page)
+$f3->route('GET /', function() {
+    //echo "Hello";
+    $view = new Template();
+    echo $view->render('views/admin.html');
+});
+
+//define a default route(confirmation page)
+$f3->route('GET /', function() {
+    //echo "Hello";
+    $view = new Template();
+    echo $view->render('views/confirmation.html');
+});
+
+//define a default route(login page)
+$f3->route('GET /', function() {
+    //echo "Hello";
+    $view = new Template();
+    echo $view->render('views/login.html');
+});
+
+//define a default route(order page)
+$f3->route('GET /', function() {
+    //echo "Hello";
+    $view = new Template();
+    echo $view->render('views/order.html');
 });
