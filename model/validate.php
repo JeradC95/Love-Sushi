675c4f85434eb9ae127726d687ec5257bb0ed0bf
Tiffany Ferderer <tferderer@mail.greenriver.edu>
2021-02-23 17:39:16 -0500

    Validation functions added

--- model/validate.php
+++ model/validate.php
@@ -4,66 +4,43 @@
      *
      */
 
-class Validate
-{
-    private $_dataLayer;
 
-    function __construct()
-    {
-        $this->_dataLayer = new DataLayer();
+//contains validation functions
+/** validName() returns true if Name is not empty */
+function validName($name) {
+    return !empty($name && ctype_alpha($name));}
+
+/** validPhone returns true if phone is only numbers and is 9 characters long
+ * @param $phone string phone number
+ * @return bool valid number
+ */
+function validPhone($phone) {
+    if(strlen($phone)==10) {
+        return is_numeric($phone);
     }
-
-    /** validFood() returns true if food is not empty and
-     * contains only letters
-     * @param String $food
-     * @return boolean
-     */
-    function validFood($food)
-    {
-        //$validFoods = array("tacos", "eggs", "pizza");
-        // && in_array(strtolower($food), $validFoods);
-
-        /*
-        if (!empty($food) && ctype_alpha($food))
-            return true;
-        else
-            return false;
-        */
-
-        return !empty($food) && ctype_alpha($food);
-    }
-
-    /** validMeal() returns true if the selected meal is in the list
-     * of valid options
-     * @param String $meal
-     * @return boolean
-     */
-    function validMeal($meal)
-    {
-        $validMeals = $this->_dataLayer->getMeals();
-        return in_array($meal, $validMeals);
-    }
-
-    /** validCondiments() returns true if all of the condiments are
-     * in the list of valid options
-     * @param Array $selectedConds
-     * @return boolean
-     */
-    function validCondiments($selectedConds)
-    {
-        //Get valid condiments from data layer
-        $validConds = $this->_dataLayer->getCondiments();
-
-        //Check every selected condiment
-        foreach ($selectedConds as $selected) {
-
-            //If the selected condiment is not in the valid list, return false
-            if (!in_array($selected, $validConds)) {
-                return false;
-            }
-        }
-
-        //If we haven't false by now, we're good!
-        return true;
-    }
+    return false;
 }
+
+/** validEmail returns true if email contains @ and .
+ *  @param $email string
+ * @return bool
+ */
+function validEmail($email) {
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    return (filter_var($email, FILTER_VALIDATE_EMAIL));
+
+}
+
+
+    /** validChoice() returns true if the selected roll is in the list
+     * of valid options
+     * @param String $selected
+     * @param $foodArray
+     * @return boolean
+     */
+    function validChoice($selected, $foodArray)
+    {
+        $validChoice = $foodArray;
+        return (in_array($selected, $validChoice));;
+    }
+
